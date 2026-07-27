1b9f6ccde0d6e2f497485a76485a0d048e25a5da
prachiti-2005 <138200137+prachiti-2005@users.noreply.github.com>
2026-07-27 22:53:32 +0530

    Update TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,12 +4,12 @@ export default function TopBar({ right }) {
   return (
     <header className="fb-topbar">
       <div className="fb-workspace-switcher">
-        <span className="fb-icon fb-icon-building" />
-          Gracia Advisory Group
+        <ion-icon name="business-outline" class="fb-icon"></ion-icon>
+        Gracia Advisory Group
         <ion-icon name="chevron-down-outline" class="fb-sidebar__chevron"></ion-icon>
       </div>
       <div className="fb-workspace-switcher">
-        <span className="fb-icon fb-icon-building" />
+        <ion-icon name="business-outline" class="fb-icon"></ion-icon>
         ABC Advisory Group
         <ion-icon name="chevron-down-outline" class="fb-sidebar__chevron"></ion-icon>
       </div>
@@ -18,7 +18,7 @@ export default function TopBar({ right }) {
 
       <span className="fb-topbar__bell">
         <ion-icon name="notifications-outline"></ion-icon>
-      <span className="fb-topbar__badge">2</span>
+        <span className="fb-topbar__badge">2</span>
       </span>
       <span className="fb-topbar__avatar" />
     </header>
